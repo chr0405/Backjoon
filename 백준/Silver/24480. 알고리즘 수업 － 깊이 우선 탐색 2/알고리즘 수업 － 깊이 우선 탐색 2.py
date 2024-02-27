3155a0cb139d5be8194471d51242f8545b33c698
chr0405 <129362281+chr0405@users.noreply.github.com>
2024-02-27 23:15:06 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 580 ms, Memory: 67996 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -0,0 +1,29 @@
+import sys
+sys.setrecursionlimit(200000)
+input = sys.stdin.readline
+
+N, M, R = map(int, input().split())
+
+ConnectList = [[] for _ in range(N+1)]
+CheckList = [0]*(N+1) # 범위가 N까지로 설정됨
+Count = 1
+
+for _ in range(M):
+    i, j = map(int, input().split())
+    ConnectList[i].append(j)
+    ConnectList[j].append(i)
+
+# DFS
+def DFS(v):
+    global Count
+    CheckList[v] = Count
+    ConnectList[v].sort(reverse=True)
+    for _ in ConnectList[v]:
+        if CheckList[_] == 0:
+            Count += 1
+            DFS(_)
+
+DFS(R)
+
+for _ in range(1, N+1):
+    print(CheckList[_])
