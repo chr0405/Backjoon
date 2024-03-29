f554cad221d900c306451988359bee75933ba5b8
chr0405 <129362281+chr0405@users.noreply.github.com>
2024-03-29 22:50:59 +0900

    [Silver IV] Title: 가로수, Time: 124 ms, Memory: 37096 KB -BaekjoonHub

--- 백준/Silver/2485. 가로수/가로수.py
+++ 백준/Silver/2485. 가로수/가로수.py
@@ -2,9 +2,7 @@ import sys
 from math import gcd
 
 trees = int(sys.stdin.readline())
-
 firstTree = int(sys.stdin.readline())
-
 treesLength = []
 
 for i in range(trees-1):
@@ -12,14 +10,14 @@ for i in range(trees-1):
     treesLength.append(num - firstTree)
     firstTree = num
 
-g = treesLength[0]
+greatest = treesLength[0]
 
-for j in range(1, len(treesLength)):
-    g = gcd(g, treesLength[j])
+for i in range(1, len(treesLength)):
+    greatest = gcd(greatest, treesLength[i])
 
 result = 0
 
 for each in treesLength:
-    result += each // g - 1
+    result += each // greatest - 1
 
 print(result)
