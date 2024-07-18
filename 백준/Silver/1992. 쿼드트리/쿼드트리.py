555ee35ba513a4a8732db7f821c1cf4adf8d780a
chr0405 <129362281+chr0405@users.noreply.github.com>
2024-07-18 22:08:07 +0900

    [Silver I] Title: 쿼드트리, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.py
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.py
@@ -1,25 +1,26 @@
-def DAQ(X, Y, N):
-    Check = N_list[X][Y]
-    for i in range(X, X+N):
-        for j in range(Y, Y+N):
-            if Check != N_list[i][j]:
-                Check = -1
-                break
-    
-    if Check == -1:
-        print("(", end='')
-        N = N // 2
-        DAQ(X, Y, N)
-        DAQ(X, Y + N, N)
-        DAQ(X + N, Y, N)
-        DAQ(X + N, Y + N, N)
-        print(")", end='')
-    
-    elif Check == 1 : print(1, end='')
-    else : print(0, end='')
-        
-N = int(input())
-N_list = [list(map(int, input())) for i in range(N)]
-# 2차원 배열
+import sys
 
-DAQ(0, 0, N)
+N = int(sys.stdin.readline().strip())
+paper = [list(map(int, sys.stdin.readline().strip())) for _ in range(N)]
+
+def check_paper(paper, x, y, size):
+    color = paper[x][y]
+    for i in range(x, x + size):
+        for j in range(y, y + size):
+            if paper[i][j] != color:
+                return False
+    return True
+
+def quad_tree(paper, x, y, size):
+    if check_paper(paper, x, y, size):
+        return str(paper[x][y])
+    else:
+        half = size // 2
+        top_left = quad_tree(paper, x, y, half)
+        top_right = quad_tree(paper, x, y + half, half)
+        bottom_left = quad_tree(paper, x + half, y, half)
+        bottom_right = quad_tree(paper, x + half, y + half, half)
+        return f"({top_left}{top_right}{bottom_left}{bottom_right})"
+
+result = quad_tree(paper, 0, 0, N)
+print(result)
