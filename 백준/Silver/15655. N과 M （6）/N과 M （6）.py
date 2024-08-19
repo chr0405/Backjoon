db92f8a3dcd49b6697b63b595065b80e7608de8f
chr0405 <129362281+chr0405@users.noreply.github.com>
2024-08-19 20:28:39 +0900

    [Silver III] Title: N과 M (6), Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.py
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -1,18 +1,18 @@
 import sys
 
 N, M = map(int, sys.stdin.readline().split())
-arr = sorted(map(int, sys.stdin.readline().split()))
-
+array = list(map(int, sys.stdin.readline().split()))
+array.sort()
 selected = []
 
-def backtrack(start):
-    if len(selected) == M:
-        print(' '.join(map(str, selected)))
+def backTracking(start) :
+    if len(selected) == M :
+        print(" ".join(map(str, selected)))
         return
     
     for i in range(start, N):
-        selected.append(arr[i])
-        backtrack(i + 1)
-        selected.pop()
+            selected.append(array[i])
+            backTracking(i + 1)
+            selected.pop()
 
-backtrack(0)
+backTracking(0)
