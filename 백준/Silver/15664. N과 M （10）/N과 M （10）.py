f802381ad13c62ee9a9b95112879dbea4cfe774c
chr0405 <129362281+chr0405@users.noreply.github.com>
2024-08-24 01:26:15 +0900

    [Silver II] Title: N과 M (10), Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15664. N과 M （10）/N과 M （10）.py
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -1,25 +1,23 @@
 import sys
 
 N, M = map(int, sys.stdin.readline().split())
-arr = list(map(int, sys.stdin.readline().split()))
-arr.sort()
+array = list(map(int, sys.stdin.readline().split()))
+array.sort()
+selected = []
 
-visited = [False] * N
-result = []
 
-def backtrack(depth, start):
-    if depth == M:
-        print(' '.join(map(str, result)))
+def backTracking(start):
+    if len(selected) == M:
+        print(" ".join(map(str, selected)))
         return
     
-    prev = -1
+    last_used = None
+    
     for i in range(start, N):
-        if not visited[i] and arr[i] != prev:
-            visited[i] = True
-            result.append(arr[i])
-            backtrack(depth + 1, i + 1)
-            result.pop()
-            visited[i] = False
-            prev = arr[i]
+        if array[i] != last_used :
+            selected.append(array[i])
+            last_used = array[i]
+            backTracking(i + 1)
+            selected.pop()
 
-backtrack(0, 0)
+backTracking(0)
