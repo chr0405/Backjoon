00f372f0678fce1ae57d7a8e30f3a98a0c2d4a0a
chr0405 <129362281+chr0405@users.noreply.github.com>
2024-08-30 21:34:44 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 1128 ms, Memory: 154932 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.py
@@ -0,0 +1,12 @@
+import sys
+
+N = int(sys.stdin.readline().strip())
+numbers = []
+
+for _ in range(N):
+    num = int(sys.stdin.readline().strip())
+    numbers.append(num)
+
+numbers.sort()
+
+print("\n".join(map(str, numbers[::-1])))
