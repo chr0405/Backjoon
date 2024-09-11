70bfbf367d72aab89301c43bc3112567db2aa104
chr0405 <129362281+chr0405@users.noreply.github.com>
2024-09-11 21:10:35 +0900

    [Silver III] Title: 계단 오르기, Time: 28 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -0,0 +1,23 @@
+import sys
+
+def max_stair_score(n, scores):
+    if n == 1:
+        return scores[0]
+    elif n == 2:
+        return scores[0] + scores[1]
+
+    dp = [0] * n
+
+    dp[0] = scores[0]
+    dp[1] = scores[0] + scores[1]
+    dp[2] = max(scores[0] + scores[2], scores[1] + scores[2])
+
+    for i in range(3, n):
+        dp[i] = max(dp[i-2] + scores[i], dp[i-3] + scores[i-1] + scores[i])
+
+    return dp[n-1]
+
+n = int(sys.stdin.readline().strip())
+scores = [int(sys.stdin.readline().strip()) for _ in range(n)]
+
+print(max_stair_score(n, scores))
