d9328b9039bbd8f70f30c09cc1d6440813ec17b8
chr0405 <129362281+chr0405@users.noreply.github.com>
2024-09-21 00:51:53 +0900

    [Silver III] Title: 2×n 타일링, Time: 32 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -2,19 +2,15 @@ import sys
 
 n = int(sys.stdin.readline().strip())
 
-def tile_count(n):
-    if n == 1:
-        return 1
-    elif n == 2:
-        return 2
-    
-    dp = [0] * (n + 1)
-    dp[1] = 1
-    dp[2] = 2
-    
-    for i in range(3, n + 1):
-        dp[i] = (dp[i-1] + dp[i-2]) % 10007
-    
-    return dp[n]
+D = [0] * (n + 1)
+D[1] = 1
 
-print(tile_count(n))
+if n >= 2:
+    D[2] = 2
+
+if n == 1:
+    print(1)
+else:
+    for i in range(3, n + 1):
+        D[i] = (D[i-1] + D[i-2]) % 10007
+    print(D[n])
