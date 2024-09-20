04cd1ae8cb9fa7a755948c4d85e33cf09670d14b
chr0405 <129362281+chr0405@users.noreply.github.com>
2024-09-20 21:05:33 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,18 +1,15 @@
 import sys
 
-def count_ways(n):
-    dp = [0] * (12)
-    
-    dp[1] = 1
-    dp[2] = 2
-    dp[3] = 4
-    
-    for i in range(4, 12):
-        dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
-    
-    return dp[n]
+T = int(sys.stdin.readline().strip()) # test case 갯수
+dq = [0] * 12
 
-T = int(sys.stdin.readline().strip())
+dq[1] = 1
+dq[2] = 2
+dq[3] = 4
+
+for i in range(4, 12):
+    dq[i] = dq[i-1]+dq[i-2]+dq[i-3]
+
 for _ in range(T):
     n = int(sys.stdin.readline().strip())
-    print(count_ways(n))
+    print(dq[n])
