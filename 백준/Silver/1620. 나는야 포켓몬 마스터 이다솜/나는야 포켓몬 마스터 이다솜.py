c0917b4d99898e49a49cbe5d1b705dddef6a14a1
chr0405 <129362281+chr0405@users.noreply.github.com>
2024-09-22 14:46:05 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 196 ms, Memory: 51260 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -1,19 +1,18 @@
 import sys
-
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
+N, M = map(int, input().split())
+pokedex_num_to_name = {} 
+pokedex_name_to_num = {}  
 
-dict = {}
+for i in range(1, N + 1):
+    name = input().strip()
+    pokedex_num_to_name[i] = name
+    pokedex_name_to_num[name] = i
 
-for i in range(1, n + 1):
-    a = input().rstrip()
-    dict[i] = a
-    dict[a] = i
-
-for i in range(m):
-    quest = input().rstrip()
-    if quest.isdigit():
-        print(dict[int(quest)])
+for _ in range(M):
+    query = input().strip()
+    if query.isdigit():
+        print(pokedex_num_to_name[int(query)]) 
     else:
-        print(dict[quest])
+        print(pokedex_name_to_num[query])
