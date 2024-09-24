791d76680ceb4fbc52d4b7f5a791a8b10ba795d8
chr0405 <129362281+chr0405@users.noreply.github.com>
2024-09-24 23:51:50 +0900

    [Silver III] Title: 빈도 정렬, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.py
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.py
@@ -1,17 +1,23 @@
-from collections import Counter
 import sys
 input = sys.stdin.readline
 
 N, K = map(int, input().split())
 sequence = list(map(int, input().split()))
 
-freq = Counter(sequence)
+seen = set()
+unique_numbers = []
+for num in sequence:
+    if num not in seen:
+        seen.add(num)
+        unique_numbers.append(num)
 
-order = {}
-for index, num in enumerate(sequence):
-    if num not in order:
-        order[num] = index
+count_dict = {}
+for num in sequence:
+    if num in count_dict:
+        count_dict[num] += 1
+    else:
+        count_dict[num] = 1
 
-sorted_numbers = sorted(sequence, key=lambda x: (-freq[x], order[x]))
+sorted_numbers = sorted(sequence, key=lambda x: (-count_dict[x], unique_numbers.index(x)))
 
 print(" ".join(map(str, sorted_numbers)))
