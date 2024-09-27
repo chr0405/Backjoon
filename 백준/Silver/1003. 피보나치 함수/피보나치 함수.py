22c8e996c182ad10fa9b47b5695fb1d5f5c5a7fd
chr0405 <129362281+chr0405@users.noreply.github.com>
2024-09-28 00:32:14 +0900

    [Silver III] Title: 피보나치 함수, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,15 +1,24 @@
 import sys
+input = sys.stdin.readline
 
-t = int(sys.stdin.readline().strip())
+testCase = int(input().strip())
+pArray = [[0, 0] for _ in range(41)]
 
-dp = [[0, 0] for _ in range(41)]
-dp[0] = [1, 0] 
-dp[1] = [0, 1] 
+pArray[0][0] = 1
+pArray[0][1] = 0
+pArray[1][0] = 0
+pArray[1][1] = 1
 
-for i in range(2, 41):
-    dp[i][0] = dp[i-1][0] + dp[i-2][0] 
-    dp[i][1] = dp[i-1][1] + dp[i-2][1]
-
-for _ in range(t):
-    n = int(sys.stdin.readline().strip())
-    print(dp[n][0], dp[n][1])
+for _ in range(testCase):
+    N = int(input().strip())
+    
+    if N == 0:
+        print(pArray[0][0], pArray[0][1])
+    elif N == 1:
+        print(pArray[1][0], pArray[1][1])
+    else:
+        for i in range(2, N + 1):
+            pArray[i][0] = pArray[i - 1][0] + pArray[i - 2][0]
+            pArray[i][1] = pArray[i - 1][1] + pArray[i - 2][1]
+        print(pArray[N][0], pArray[N][1])
+    
