819407eb41c571e9fdc51073688e6db5a98179f3
chr0405 <129362281+chr0405@users.noreply.github.com>
2024-10-14 20:28:18 +0900

    [Silver IV] Title: 동전 0, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,23 +1,17 @@
-import sys
-
-# 동전의 종류(개수는 제한 없음), 금액
-N, K = map(int, sys.stdin.readline().split())
-coins = []
+N, K = map(int, input().split())
+coinList = []
+count = 0
 
 for _ in range(N):
-    coins.append(int(sys.stdin.readline().strip()))
+    coin = int(input().strip())
+    coinList.append(coin)
+    
+coinList.sort(reverse=True)
 
-# 큰 순으로
-coins.sort(reverse=True)
-
-count = 0
-for coin in coins:
-    if K == 0:
-        break
-
-    # 몫이 0이면(= 동전의 가치가 금액보다 크면) count가 변경되지 않음
-    count += K // coin
-    # 동전의 가치가 금액보다 크면 K가 변경되지 않음
-    K %= coin
+for coin in coinList:
+    quot = K // coin
+    if quot >= 1:
+        count += quot
+        K %= coin
 
 print(count)
