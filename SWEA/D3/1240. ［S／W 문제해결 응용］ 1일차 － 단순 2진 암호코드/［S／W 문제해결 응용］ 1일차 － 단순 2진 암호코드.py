1733adbd4e3891023e05b345f5e5210fe9a7e332
chr0405 <129362281+chr0405@users.noreply.github.com>
2024-11-06 00:38:32 +0900

    [D3] Title: [S/W 문제해결 응용] 1일차 - 단순 2진 암호코드, Time: 125 ms, Memory: 45,080 KB -BaekjoonHub

--- SWEA/D3/1240. ［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드/［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드.py
+++ SWEA/D3/1240. ［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드/［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드.py
@@ -1,47 +1,38 @@
 T = int(input().strip())
 
-scanner = {
-    "0001101": 0, "0011001": 1, "0010011": 2,
-    "0111101": 3, "0100011": 4, "0110001": 5,
-    "0101111": 6, "0111011": 7, "0110111": 8,
-    "0001011": 9
+numberDict = {
+    '0001101': 0, '0011001': 1, '0010011': 2,
+    '0111101': 3, '0100011': 4, '0110001': 5,
+    '0101111': 6, '0111011': 7, '0110111': 8,
+    '0001011': 9
 }
 
-for tc in range(1, T + 1):
+for i in range(1, T + 1):
     N, M = map(int, input().split())
     
-    maze = []
-    for _ in range(N):
-        row = input().strip()
-        if "1" in row:  # '1'이 포함된 행만 저장
-            maze.append(row)
-    
-    if not maze:
-        print(f"#{tc} 0")
-        continue
-    
-    # 마지막 행을 사용하여 오른쪽 끝에서 첫 '1'의 위치를 찾음
-    code_line = maze[-1]
-    end = code_line.rfind("1")
-    code_line = code_line[end - 55:end + 1]  # 56비트(7비트 * 8개)를 가져옴
-
-    ans = []
-    for i in range(0, 56, 7):
-        segment = code_line[i:i + 7]
-        if segment in scanner:
-            ans.append(scanner[segment])
-        else:
-            ans = []
+    totalArray = [input().strip() for _ in range(N)]
+    codeArray = None
+    for row in totalArray:
+        if '1' in row:  # 암호 코드가 있는 줄 찾기
+            last_one_index = row.rfind('1')
+            codeArray = row[last_one_index - 55:last_one_index + 1]
             break
     
-    if len(ans) == 8:
-        odd_sum = sum(ans[0::2])
-        even_sum = sum(ans[1::2])
-        result = odd_sum * 3 + even_sum
+    # 암호를 7자리씩 분리하여 숫자로 변환
+    decoded_digits = []
+    for j in range(0, 56, 7):
+        segment = codeArray[j:j+7]
+        if segment in numberDict:
+            decoded_digits.append(numberDict[segment])
+    
+    # 홀수 자리와 짝수 자리 합산
+    oddCount = sum(decoded_digits[k] for k in range(0, len(decoded_digits), 2))
+    evenCount = sum(decoded_digits[k] for k in range(1, len(decoded_digits), 2))
 
-        if result % 10 == 0:
-            print(f"#{tc} {sum(ans)}")
-        else:
-            print(f"#{tc} 0")
+    # 검증 코드
+    if (oddCount * 3 + evenCount) % 10 == 0:
+        answer = sum(decoded_digits)
     else:
-        print(f"#{tc} 0")
+        answer = 0
+    
+    print(f'#{i} {answer}')
