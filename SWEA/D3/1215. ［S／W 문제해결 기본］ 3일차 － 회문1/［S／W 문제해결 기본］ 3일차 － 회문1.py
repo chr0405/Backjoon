107ebb24f37ebcb87d13b29ca1f260e413beb208
chr0405 <129362281+chr0405@users.noreply.github.com>
2024-11-06 16:35:10 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문1, Time: 102 ms, Memory: 44,016 KB -BaekjoonHub

--- SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.py
+++ SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.py
@@ -1,21 +1,15 @@
-def isPalindrome(word):
-    return word == word[::-1]
+def checkDef(checkList):
+    return 1 if checkList == checkList[::-1] else 0
 
-for testCase in range(1, 10 + 1):
-    length = int(input().strip())  
-    matrix = [list(input().strip()) for _ in range(8)] 
-    answer = 0
-
-    for row in range(8):
-        for col in range(8 - length + 1):
-            word = matrix[row][col:col + length]
-            if isPalindrome(word):
-                answer += 1
-
-    for col in range(8):
-        for row in range(8 - length + 1): 
-            word = [matrix[row + i][col] for i in range(length)]  
-            if isPalindrome(word):
-                answer += 1
-
-    print(f'#{testCase} {answer}')
+for tc in range(1, 11):
+    wordLen = int(input().strip())
+    wordBoard = [list(input().strip()) for _ in range(8)]
+    wordBoardReverse = list(zip(*wordBoard))
+    count = 0
+    
+    for i in range(8):
+        for j in range(8 - wordLen + 1):
+            count += checkDef(wordBoard[i][j:j + wordLen])
+            count += checkDef(wordBoardReverse[i][j:j + wordLen])
+    
+    print(f'#{tc} {count}')
